757fb11d7ecf6e1079e19c64952a5872155a5933
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-05-21 11:59:56 +0300

    modified  lib/app.py

--- lib/app.py
+++ lib/app.py
@@ -1,9 +1,2 @@
-from __future__ import print_function
-
-import sys
-
-if sys.version_info[0] < 3:
-    print(u"Hello World! Pass this test, please.")
-else:
-    print("Hello World! Pass this test, please.")
+print("Hello World! Pass this test, please.")
 
